ae165dc60e0f8adf85cd0f76639140b15005c9e7
odelgi <olivia.delgiorgio@mail.mcgill.ca>
2020-04-17 16:04:23 -0400

    Continue developing results merging. Now at least for troubleshooting processing

--- table_merge.py
+++ table_merge.py
@@ -2,20 +2,50 @@ import arcpy
 import os
 import pandas as pd
 from collections import defaultdict
-from internal_functions import *
+from accesscalc_parallel import *
 
-formatdir = os.path.join(os.path.dirname(os.path.abspath(__file__)).split('\\src')[0])  # To update for final run
-resdir = os.path.join(formatdir, 'results')
-outstats = os.path.join(resdir, 'outstats.gdb')
+def tabmerge_dict(tablist):
+    outdict = {}
+    for tab in tablist:
+        print(tab)
+        for row in arcpy.da.SearchCursor(tab, ['Value', 'MEAN']):
+            outdict[row[0]] = row[1]
+    return(outdict)
 
-tablist = getfilelist(outstats, '*')
-tables_pd = pd.concat(pd.Series(tablist),
-                      pd.Series(tablist).str.split('_', expand=True),
-                      axis=1)
-tables_pd.columns = ['path', 'llhood', 'year', 'weighting', 'group']
+if __name__ == 'main':
+    formatdir = os.path.join(os.path.dirname(os.path.abspath(__file__)).split('\\src')[0])  # To update for final run
+    resdir = os.path.join(formatdir, 'results')
+    outstats = os.path.join(resdir, 'outstats.gdb')
+    outmerge = os.path.join(resdir, 'outstats_merge.gdb')
+    pathcheckcreate(outmerge)
+
+    #Get panda df of tables
+    #tablist = getfilelist(resdir, '.*[.]dbf')
+    arcpy.env.workspace = resdir
+    tablist = arcpy.ListTables(table_type='dBASE')
+
+    tables_pd = pd.concat([pd.Series(tablist),
+                          pd.Series(tablist).str.split('_', expand=True)],
+                          axis=1)
+    tables_pd.columns = ['path', 'dataset', 'llhood1', 'llhood2', 'year', 'weighting', 'group']
+
+    tables_pd.groupby('year')['path'].nunique()
+
+    merged_dict = {}
+    for llhood in tables_pd['llhood1'].unique():
+        for year in tables_pd['year'].unique():
+            print(year)
+            merged_dict[llhood+year] = tabmerge_dict(tables_pd.loc[(tables_pd['llhood1']==llhood) &
+                                                                   (tables_pd['year']==year), 'path'])
+
+    for key, values in merged_dict.iteritems():
+        print(key)
+        print(len(values))
 
 
-
+##########################################################################################################################
+##########################################################################################################################
+##########################################################################################################################
 # Process: Merge tables and join to points (within loop)
 for year in analysis_years:
     print(year)
